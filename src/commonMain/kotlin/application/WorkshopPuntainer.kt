da95d6e72a5afab4c4a864727115c612b9a5ac49
candenizcam <e193777@metu.edu.tr>
2022-01-29 17:55:50 +0300

    mükemmel oldu

--- src/commonMain/kotlin/application/WorkshopPuntainer.kt
+++ src/commonMain/kotlin/application/WorkshopPuntainer.kt
@@ -1,8 +1,11 @@
 package application
 
 import com.soywiz.korim.bitmap.Bitmap
+import com.soywiz.korim.bitmap.Bitmap32
+import com.soywiz.korim.bitmap.context2d
 import com.soywiz.korim.format.readBitmap
 import com.soywiz.korio.file.std.resourcesVfs
+import com.soywiz.korma.geom.vector.rect
 import modules.basic.Colour
 import pungine.Puntainer
 import pungine.geometry2D.Rectangle
@@ -16,15 +19,11 @@ class WorkshopPuntainer private constructor(relativeRectangle: Rectangle): Punta
         private set
     val choicePos = Vector(0.5,536.0/GlobalAccess.windowSize.height)
     var onChoice = {foodId: String, choice: Int->} //choice 0-> pickle; choice 1-> jam
-    var conveyorHolder = 0.0
-
     var activeBasket: Basket? = null
     private set
     val fruitRectangle = Rectangle(Vector(0.0,0.0), 200.0/GlobalAccess.windowSize.width,200.0/GlobalAccess.windowSize.height)
 
     private suspend fun init() {
-
-
         resourcesVfs["grid44.png"].readBitmap().also {
             punImage("id", Rectangle(0.0,1.0,0.0,1.0), bitmap = it)
             punImage("fruitBasket", Rectangle(1.25,1.5,0.25,0.5), bitmap = it)
@@ -34,9 +33,44 @@ class WorkshopPuntainer private constructor(relativeRectangle: Rectangle): Punta
             punImage("logo", Rectangle(0.5-240.0/GlobalAccess.windowSize.width,0.5+240.0/GlobalAccess.windowSize.width,1.0-310.0/GlobalAccess.windowSize.height,1.0), bitmap = it)
         }
 
-        val b1 = Button("pickleButton",Rectangle(0.1,0.3,0.1,0.3),resourcesVfs["buttons/pickle_jar.png"].readBitmap())
+        val rectByPixel = GlobalAccess.virtualRect.fromRated(relativeRectangle)
 
-        val b2 = Button("jamButton",Rectangle(0.7,0.9,0.1,0.3),resourcesVfs["buttons/jam_jar.png"].readBitmap())
+        val transparentBlock = Bitmap32(10,10).context2d {
+            this.rect(0,0,10,10)
+            this.fill(Colour.rgba(0.0,0.0,0.0,0.0).korgeColor)
+        }
+
+
+
+        addPuntainer(ConveyorBeltPuntainer.create(Rectangle(0.0,1.0,360.0/rectByPixel.height,620.0/rectByPixel.height)))
+
+        resourcesVfs["workshop/Vinegar.png"].readBitmap().also {
+            punImage("vinegar", rectByPixel.toRated(Rectangle(Vector(64.0,400.0),240.0,360.0,Rectangle.Corners.TOP_LEFT)), bitmap = it)
+        }
+
+        resourcesVfs["workshop/Sugar.png"].readBitmap().also {
+            punImage("sugar", rectByPixel.toRated(Rectangle(Vector(976.0,400.0),240.0,360.0,Rectangle.Corners.TOP_LEFT)), bitmap = it)
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        addPuntainer(JarPuntainer.create(
+            rectByPixel.toRated(Rectangle(Vector(520.0,700.0),240.0,280.0,Rectangle.Corners.TOP_LEFT)))
+        )
+
+        val b1 = Button("pickleButton",Rectangle(0.0,0.4,0.0,620.0/rectByPixel.height),transparentBlock,transparentBlock,hoverBitmap = resourcesVfs["buttons/pickle_jar.png"].readBitmap())
+
+        val b2 = Button("jamButton",Rectangle(0.6,1.0,0.0,620.0/rectByPixel.height),transparentBlock,transparentBlock,hoverBitmap = resourcesVfs["buttons/jam_jar.png"].readBitmap())
 
         b1.clickFunction = {
             if(GlobalAccess.gameState.vinegar>0){
@@ -52,26 +86,13 @@ class WorkshopPuntainer private constructor(relativeRectangle: Rectangle): Punta
         addPuntainer(b1)
         addPuntainer(b2)
 
-        val rectByPixel = GlobalAccess.virtualRect.fromRated(relativeRectangle)
-
-
-        addPuntainer(ConveyorBeltPuntainer.create(Rectangle(0.0,1.0,360.0/rectByPixel.height,620.0/rectByPixel.height)))
-
-
-
-
-
-        addPuntainer(JarPuntainer.create(
-            rectByPixel.toRated(Rectangle(Vector(520.0,700.0),240.0,280.0,Rectangle.Corners.TOP_LEFT)))
-        )
-        println("jarring")
-
 
 
     }
 
     fun picklePressed(){
         if(choicePos==fruitPos){
+            buttonsActive(false)
             (puntainers.first { it.id == "fruitBasket" } as PunImage).colorMul = Colour.GREEN.korgeColor
             activeBasket!!.status = 0
             onChoice(activeBasket!!.id,activeBasket!!.status)
@@ -81,6 +102,7 @@ class WorkshopPuntainer private constructor(relativeRectangle: Rectangle): Punta
 
     fun jamPressed(){
         if(choicePos==fruitPos){
+            buttonsActive(false)
             (puntainers.first { it.id == "fruitBasket" } as PunImage).colorMul = Colour.RED.korgeColor
             activeBasket!!.status = 1
             onChoice(activeBasket!!.id,activeBasket!!.status)
@@ -89,7 +111,6 @@ class WorkshopPuntainer private constructor(relativeRectangle: Rectangle): Punta
     }
 
     fun updateClockBySec(sec: Int){
-
         (puntainers.first { it.id == "clockPuntainer" } as ClockPuntainer).setTimeAsSeconds(sec.toInt())
     }
 
@@ -109,18 +130,14 @@ class WorkshopPuntainer private constructor(relativeRectangle: Rectangle): Punta
             removeChild(it)
             activeBasket = fruitList.random().copy()
             punImage("fruitBasket",Rectangle(Vector(0.5,836.0/GlobalAccess.windowSize.height),fruitRectangle.width,fruitRectangle.height),activeBasket!!.bitmap)
-
-
-
         }
         val toUp = puntainers.first { it.id=="jarPuntainer" }
         removeChild(toUp)
         addChild(toUp)
-
         fruitPos= Vector(0.5,836.0/GlobalAccess.windowSize.height)
-
-
     }
+
+
 
 
     /** This moves by step in relative fashion
@@ -135,6 +152,7 @@ class WorkshopPuntainer private constructor(relativeRectangle: Rectangle): Punta
             (puntainers.first { it.id == "conveyorBeltPuntainer" } as ConveyorBeltPuntainer).update( x.toDouble())
         }else if(y!=0){
             if(fruitPos.y-horizontalStep*0.5<= choicePos.y){
+                buttonsActive(true)
                 fruitPos = Vector(fruitPos.x,choicePos.y)
                 return
             }
@@ -150,15 +168,15 @@ class WorkshopPuntainer private constructor(relativeRectangle: Rectangle): Punta
         puntainers.first { it.id == "fruitBasket" }.also {
             it.resizeRect(Rectangle(fruitPos,it.relativeRectangle.width,it.relativeRectangle.height))
         }
-
-
-
-
-        //(puntainers.first { it.id == "conveyorBeltPuntainer" } as ConveyorBeltPuntainer).update( x.toDouble())
-
-
     }
 
+    fun buttonsActive(b: Boolean){
+        (puntainers.first { it.id=="jamButton" } as Button).inactive = !b
+        (puntainers.first { it.id=="pickleButton" } as Button).inactive = !b
+    }
+
+    // alttakini tercihen kullanmayın
+    /*
     fun moveOnConveyor(setX: Double=fruitPos.x, setY: Double=fruitPos.y){
         if(setX!=fruitPos.x){
             //(puntainers.first { it.id == "conveyorBeltPuntainer" } as ConveyorBeltPuntainer).update( (setX-jarPos.x)*10.0)
@@ -177,10 +195,9 @@ class WorkshopPuntainer private constructor(relativeRectangle: Rectangle): Punta
         puntainers.first { it.id == "fruitBasket" }.also {
             it.resizeRect(Rectangle(fruitPos,it.relativeRectangle.width,it.relativeRectangle.height))
         }
-
-
-
     }
+
+     */
 
     data class Basket(val id: String, val bitmap: Bitmap, var status: Int = -1)
 
